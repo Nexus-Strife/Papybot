bc5c8b69d3ba4f3bc415c491c1bff05d497b0796
Nexus-Strife <34130887+Nexus-Strife@users.noreply.github.com>
2018-08-17 19:42:16 +0200

    Intégration de google maps

--- Flask/views.py
+++ Flask/views.py
@@ -1,11 +1,48 @@
-from flask import Flask, render_template  # Import of the flask's lib
+from flask import Flask, render_template, request  # Import of the flask's lib
+from flask_googlemaps import GoogleMaps
+import json
+import webbrowser
+
 
 app = Flask(__name__)
 
+GoogleMaps(app, key='AIzaSyDPEk_9cUI_Z9aeLSMn-JnnXKFjvxW7r-s')
 
-@app.route('/')
+
+cities = ['Paris']
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")  # Display index.html which contain the user interface
+    return render_template("index.html")  # Display index.html which contain the UI
+
+
+def mapview():
+    # creating a map in the view
+    mymap = Map(
+        identifier="view-map",
+    )
+    return render_template('index.html', mymap=mymap)
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    txt = request.form['research'];
+    if txt in cities:
+        webbrowser.open('https://google.fr/maps/place/' + txt)
+    else:
+        pass
+    return json.dumps({'status': 'OK', 'txt': txt});
+
+
+@app.route("/resultat", methods=['POST'])
+def resultat():
+    txt = request.form['research'];
+    if txt in cities:
+        webbrowser.open('http://google.fr/maps/place/' + txt)
+    else:
+        pass
+    return json.dumps({'status': 'OK', 'txt': txt});
 
 
 if __name__ == "__main__":
