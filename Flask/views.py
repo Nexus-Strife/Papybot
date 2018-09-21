6301ad1fd4b25c3be76f72455c143cb90478fb99
Nexus-Strife <34130887+Nexus-Strife@users.noreply.github.com>
2018-09-21 15:54:17 +0200

    Le grand changement

--- Flask/views.py
+++ Flask/views.py
@@ -1,49 +1,76 @@
 from flask import Flask, render_template, request  # Import of the flask's lib
 from flask_googlemaps import GoogleMaps
 import json
-import webbrowser
+import wikipediaapi
 
 
-app = Flask(__name__)
+app = Flask(__name__) # Initialization of Flask
 
 GoogleMaps(app, key='AIzaSyDPEk_9cUI_Z9aeLSMn-JnnXKFjvxW7r-s')
 
-
-cities = ['Paris']
+wiki_wiki = wikipediaapi.Wikipedia('fr')  # Initialization of Wikipedia API
+page_paris = wiki_wiki.page("Nintendo")
+city = ["Paris"]
+susp = "..."
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")  # Display index.html which contain the UI
 
+    paris = "Paris"
 
-def mapview():
-    # creating a map in the view
-    mymap = Map(
-        identifier="view-map",
-    )
-    return render_template('index.html', mymap=mymap)
-
-
-@app.route('/result', methods=['POST'])
-def result():
-    txt = request.form['research'];
-    if txt in cities:
-        webbrowser.open('https://google.fr/maps/place/' + txt)
+    if paris in city:
+        wiki_desc = page_paris.summary[0:295]
+        response = render_template("index.html", wiki_desc=wiki_desc)
+        return response
     else:
+        wiki_desc = page_paris.summary[0:295]
+        return render_template("index.html")
+
+
+@app.route("/update_map", methods=['GET', 'POST'])
+def update_map():
+
+    var = request.form.get('research');
+
+    if var == "":
         pass
-    return json.dumps({'status': 'OK', 'txt': txt});
 
-
-@app.route("/resultat", methods=['POST'])
-def resultat():
-    txt = request.form['research'];
-    if txt in cities:
-        webbrowser.open('http://google.fr/maps/place/' + txt)
     else:
-        pass
-    return json.dumps({'status': 'OK', 'txt': txt});
+        stop_words_json = json.loads(open("static/stop_words_fr.json").read())
+
+        for words in var.split():
+            word_lowercase = words.lower()
+            page_in_wiki = wiki_wiki.page(words)
+            exist = page_in_wiki.exists()
+
+            if exist == False:  # Si la page du mot en cours n'existe pas
+
+                if word_lowercase in stop_words_json["stop_words"]:
+                    pass
+                else:
+                    pass
+
+            else:  # Si la page du mot en cours existe
+                if word_lowercase in stop_words_json["stop_words"]:
+                    pass
+
+                else:
+
+                    page_to_read = wiki_wiki.page(words)
+                    text = page_to_read.summary[0:300]
+
+                    if not text:
+                        print("J'sais pas grand chose là d'sus mon p'tit gars")
+                        # rajouter adresse
+                    else:
+                       text_susp = text + susp
+                       print("J'vais te dire c'que j'sais mon gamin. " + text_susp)
+                       print(page_to_read.fullurl)
+                       # rajouter url wiki et adresse
+
+    return json.dumps({'status': 'OK'});
 
 
 if __name__ == "__main__":
-    app.run()  # Run local server
+    app.run(debug=True)  # Run local server. Mode debug on for developpment
